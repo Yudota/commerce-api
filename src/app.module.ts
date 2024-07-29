ce898fd911e53fe5df64200169f450c943241631
Yudota <yuri.fernandes.961@gmail.com>
2024-07-28 23:51:16 -0300

    config: configuracao de db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { LivroModule } from './livro/livro.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsuarioModule,
     LivroModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
       inject: [PostgresConfigService],
